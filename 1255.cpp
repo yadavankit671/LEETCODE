a19a781105655bf8255681e6a96cd4ac2ee15674
Ankit Yadav <official.ank671@gmail.com>
2024-05-30 22:32:25 +0530

    24th May 2024 - 1255 Modified

--- 1255.cpp
+++ 1255.cpp
@@ -1,93 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-
-
-struct VectorHash {
-    size_t operator()(const vector<int>& v) const {
-        hash<int> hasher;
-        size_t seed = 0;
-        for (int i : v) {
-            seed ^= hasher(i) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        }
-        return seed;
-    }
-};
-
 class Solution {
+    private:
+    bool  CanWordForm(vector<int> &freq,string word){
+        vector<int> temp=freq;
+        for(int i=0;i<word.size();i++){
+            if(temp[word[i]-'a'] <= 0) return false;
+            temp[word[i]-'a']--;
+        }
+        freq=temp;
+        return true;
+    }
+    int getScore(string word,vector<int> &score){
+        int res = 0;
+        for(int i=0;i<word.size();i++){
+            res += score[word[i]-'a'];
+        }
+        return res;
+    }
+    void popBack(vector<int> &freq,string word){
+        for(int i=0;i<word.size();i++){
+            freq[word[i]-'a']++;
+        }
+    }
+    int BackTrack(vector<string> &words,int index,vector<int> &freq,map<string,int> &scoreBuff,int currScore,int &result){
+        if(index>=words.size()) return currScore;
+        vector<int> tempFreq=freq;
+        for(int i=index;i<words.size();i++){
+            if(CanWordForm(tempFreq,words[i])){
+                result=max(result,BackTrack(words,i+1,tempFreq,scoreBuff,currScore+scoreBuff[words[i]],result));
+                //popBack
+                popBack(tempFreq,words[i]);
+            }
+            result=max(currScore,result);
+        }
+        return result;
+    }
 public:
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         ios_base::sync_with_stdio(false);
-        unordered_map<vector<int>, int, VectorHash> memo;
-        vector<int> letterFreq(26, 0);
-        for (char c : letters) letterFreq[c - 'a']++;
-
-        vector<pair<string, int>> wordsWithScores;
-        for (string& word : words) {
-            wordsWithScores.push_back({word, calculateScore(word, score)});
+        map<string,int> scoreBuff;
+        for(string word: words){
+            scoreBuff[word] = getScore(word,score);
         }
-
-        sort(wordsWithScores.begin(), wordsWithScores.end(),
-             [](const auto& a, const auto& b) { return a.second > b.second; });
-
-        return backtrack(wordsWithScores, letterFreq, 0, memo);
-    }
-
-private:
-    int backtrack(const vector<pair<string, int>>& words, vector<int> freq, int startIdx, 
-                  unordered_map<vector<int>, int, VectorHash>& memo) {
-
-        if (memo.count(freq)) {
-            return memo[freq];
-        }
-
-        int maxScore = 0;
-        for (int i = startIdx; i < words.size(); ++i) {
-            auto [word, wordScore] = words[i];
-            if (canFormWord(word, freq)) {
-                updateFrequency(freq, word, -1);
-                maxScore = max(maxScore, wordScore + backtrack(words, freq, i + 1, memo));
-                updateFrequency(freq, word, 1);  
-            }
-        }
-
-        memo[freq] = maxScore;
-        return maxScore;
-    }
-
-    int calculateScore(const string& word, const vector<int>& score) {
-        int totalScore = 0;
-        for (char c : word) {
-            totalScore += score[c - 'a'];
-        }
-        return totalScore;
-    }
-
-    bool canFormWord(const string& word, const vector<int>& freq) {
-    vector<int> tempFreq = freq;
-        for (char c : word) {
-            if (--tempFreq[c - 'a'] < 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
-    void updateFrequency(vector<int>& freq, const string& word, int change) {
-        for (char c : word) {
-            freq[c - 'a'] += change;
-        }
+        int result=0;
+        vector<int> freq(26,0);
+        for(char c : letters)   freq[c-'a']++;
+        result=max(result,BackTrack(words,0,freq,scoreBuff,0,result));
+        return result;
     }
 };
 
 
-
 int main(){
-    vector<string> words={"dog","cat","dad","good"};
-    vector<char> letters={'a','a','c','d','d','d','g','o','o'};
-    vector<int> score={1,0,9,5,0,0,3,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0};
+    vector<string> words={"azb","ax","awb","ayb","bpppp"};
+    vector<char> letters={'z','a','w','x','y','b','p','p','p'};
+    vector<int> score={10,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,3,2,3,3};
     Solution obj;
     cout<<"\nResult : "<<obj.maxScoreWords(words,letters,score)<<endl;
     
