742f46bc6e41e29d5b1f611289fae0795ad6b505
Ankit Yadav <official.ank671@gmail.com>
2024-06-04 10:27:59 +0530

    4th June 2024 10:28 AM

---
+++ Java Code/src/P409.java
@@ -0,0 +1,27 @@
+class Solution409 {
+    public int longestPalindrome(String s) {
+        int result=0;
+        int[] buff = new int[266];
+        for (int i=0;i<s.length();i++) buff[s.charAt(i)]+=1;
+        int x=0;
+        for(int i=0;i<buff.length;i++){
+            if(buff[i]>0){
+                if(buff[i]%2==0){
+                    result+=buff[i];
+                }
+                else {
+                    x=1;
+                    result+=buff[i]-1;
+                }
+            }
+        }
+        return (x==0)? result: result+1;
+    }
+}
+public class P409 {
+    public static void main(String[] args) {
+        String s="abccccdd";
+        Solution409 obj=new Solution409();
+        System.out.println(obj.longestPalindrome(s));
+    }
+}
