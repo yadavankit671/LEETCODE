2995f9052a1da3a90c66f408ea2f8656cb689c07
Ankit Yadav <official.ank671@gmail.com>
2024-06-29 00:51:01 +0530

    6/29/2024

--- Java Code/src/App.java
+++ Java Code/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("!!  Welcome to Ankit's LeetCode Repository !!");
+        // If you find some code that can be further optimized please let me know !! --> official.ank671@gmail.com
+        // Happy Coding!!
     }
 }
