d8970b441970a55b309c38dfb03e36ea0261e1c8
Ankit Yadav <official.ank671@gmail.com>
2024-06-30 20:20:59 +0530

    added 669

---
+++ Java Code/src/P669.java
@@ -0,0 +1,16 @@
+public class P669 {
+    private TreeNode solve(TreeNode node, int low, int high){
+        if(node==null) return null;
+        if(node.val<low) node=solve(node.right, low, high);
+        else if(node.val>high) node=solve(node.left, low,high);
+        else {
+            node.left=solve(node.left, low, high);
+            node.right=solve(node.right, low, high);
+        }
+        return node;
+    }
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        root=solve(root, low, high);
+        return root;
+    }
+}
