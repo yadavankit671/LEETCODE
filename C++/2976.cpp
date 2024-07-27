90505f4f530b60b2d7eafa0143ed5bb3bd208ba3
Ankit Yadav <official.ank671@gmail.com>
2024-07-28 02:10:02 +0530

    modified:   2976

--- C++/2976.cpp
+++ C++/2976.cpp
@@ -2,47 +2,36 @@
 using namespace std;
 class Solution {
     private:
+    const int size = 26;
     const long INF = 1e9;
-    void floydWarshall(vector<vector<long> >& dist,int n){
-        for(int k = 0; k < n; ++k){
-            for(int i = 0; i < n; ++i){
-                for(int j = 0; j < n; ++j){
+    void floydWarshall(vector<vector<long> >& dist){
+        for(int k = 0; k < size; ++k){
+            for(int i = 0; i < size; ++i){
+                for(int j = 0; j < size; ++j){
                     dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-    }
-    int findGraphSize(vector<char>& original,vector<char>& changed){
-        set<char> st(original.begin(),original.end());
-        st.insert(changed.begin(),changed.end());
-        return st.size();
     }
 public:
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        int n = findGraphSize(original,changed); // find the size of the graph // only required characters
-        vector<vector<long> > graph(n,vector<long>(n,INF)); 
-        for(int i = 0; i < n; i++) graph[i][i] = 0;
-        unordered_map<char,int> mp;
-        int index = 0;
+        vector<vector<long> > graph(size,vector<long>(size,INF)); 
+        for(int i = 0; i < size; i++) graph[i][i] = 0;
         for(int i = 0; i < cost.size(); ++i){
-            char org = original[i];
-            char tar = changed[i];
-            if(mp.find(org) == mp.end()) mp[org] = index++;
-            if(mp.find(tar) == mp.end()) mp[tar] = index++;
-            int u = mp[org];
-            int v = mp[tar];
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
             graph[u][v] = min<long>(graph[u][v], cost[i]);
         }
-        floydWarshall(graph,n);
+        floydWarshall(graph);
         long long totalCost = 0;
         for(int i = 0; i < source.length(); ++i){
-            char src = source[i];
-            char tar = target[i];
-            if(mp.find(src) == mp.end() || mp.find(tar) == mp.end() || graph[mp[src]][mp[tar]] >= INF) return -1;
-            totalCost += graph[mp[src]][mp[tar]];
+            int src = source[i] - 'a';
+            int tar = target[i] - 'a';
+            if(graph[src][tar] >= INF) return -1;
+            totalCost += graph[src][tar];
         }
         return totalCost;
     }
--- Java Code/src/P2976.java
+++ Java Code/src/P2976.java
@@ -1,51 +1,35 @@
 import java.util.*;
 
 class Solution {
+    private final int size = 26;
     private final long INF = (long) 1e9;
-    private int findGraphSize(char[] original, char[] changed) {
-        Set<Character> st = new HashSet<>();
-        int size = original.length;
-        for(int i = 0; i < size; ++i){
-            st.add(original[i]);
-            st.add(changed[i]);
-        }
-        return st.size();
-    }
     private void floydWarshall(long[][] graph){
-        int n = graph.length;
-        for(int k = 0; k < n; ++k){
-            for(int i = 0; i < n; ++i){
-                for(int j = 0; j < n; ++j){
+        for(int k = 0; k < size; ++k){
+            for(int i = 0; i < size; ++i){
+                for(int j = 0; j < size; ++j){
                     graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
                 }
             }
         }
     }
     public long minimumCost(String source, String target, char[] original, char[] changed, int[] cost) {
-        int n = findGraphSize(original,changed);
-        long[][] graph = new long[n][n];
-        for (int i = 0; i < n; i++) {
+        long[][] graph = new long[size][size];
+        for (int i = 0; i < size; i++) {
             Arrays.fill(graph[i],INF);
             graph[i][i] = 0;
         }
-        Map<Character, Integer> mp = new HashMap<>();
-        int index = 0;
         for(int i = 0; i < cost.length; ++i){
-            char org = original[i];
-            char tar = changed[i];
-            if(!mp.containsKey(org)) mp.put(org,  index++);
-            if(!mp.containsKey(tar)) mp.put(tar,  index++);
-            int u = mp.get(org);
-            int v = mp.get(tar);
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
             graph[u][v] = Math.min(graph[u][v],cost[i]);
         }
         floydWarshall(graph);
         long totalCost = 0;
         for(int i = 0; i < source.length(); ++i){
-            char src = source.charAt(i);
-            char tar = target.charAt(i);
-            if(!mp.containsKey(src) || !mp.containsKey(tar) || graph[mp.get(src)][mp.get(tar)] >= INF) return -1;
-            totalCost += graph[mp.get(src)][mp.get(tar)];
+            int u = source.charAt(i) - 'a';
+            int v = target.charAt(i) - 'a';
+            if(graph[u][v] >= INF) return -1;
+            totalCost += graph[u][v];
         }
         return totalCost;
     }
